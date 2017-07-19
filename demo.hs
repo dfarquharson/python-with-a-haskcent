bfa6995797bb68c643be4817eb86212771d1746c
David Farquharson <djfarquharson@spscommerce.com>
2017-07-19 14:14:36 -0500

    install QuickCheck for haskell fun.

--- demo.hs
+++ demo.hs
@@ -1,4 +1,7 @@
+import Test.QuickCheck (generate, elements)
+
 nats = [1,2..]
 evens = filter(\x -> x `mod` 2 == 0) nats
 odds = filter(\x -> x `mod` 2 == 1) nats
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+rand = (generate . elements)
