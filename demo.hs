87848d8dfec66fcbb6419def3ea2dd64bad08ed2
David Farquharson <djfarquharson@spscommerce.com>
2017-07-19 14:22:05 -0500

    pointfree as rand'

--- demo.hs
+++ demo.hs
@@ -4,4 +4,8 @@ nats = [1,2..]
 evens = filter(\x -> x `mod` 2 == 0) nats
 odds = filter(\x -> x `mod` 2 == 1) nats
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-rand = (generate . elements)
+
+-- explicit arg xs
+rand xs = (generate . elements) xs
+-- but we didn't need to be so explicit when we're just composing functions
+rand' = (generate . elements)
